dc3b1a8d389ad0aae013ea54c38bec4c78af49c1
Ruairi McLoughlin <ruairi.mcLoughlin@azyra.com>
2021-09-02 14:22:18 +0100

    Added semi-colons

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require("express") 
-const app = express()
+const express = require("express"); 
+const app = express();
 
 app.get("/", function(req,res)
 {
     res,send("Application Working")
-})
+});
 
 app.listen(process.env.PORT || 5000 )
